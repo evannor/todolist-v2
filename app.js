16c2788087c143851ffbbbf9ffab5fcf17ba2b97
evannor <erica.vannorman@gmail.com>
2020-10-25 15:32:44 -0700

    Create route to add and keep items on list

--- app.js
+++ app.js
@@ -58,15 +58,12 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
 
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const itemName = req.body.newItem;
+  const newItem = new Item({
+    name: itemName
+  });
+  newItem.save();
+  res.redirect("/");
 });
 
 app.get("/work", function(req, res) {
